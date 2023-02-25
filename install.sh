f995d95f05e568b971a05a82e67e6cab75a6cd6a
Nguyen Thanh Dat <37483861+DatDevCrusher@users.noreply.github.com>
2023-02-25 21:42:24 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 random() {
-tr </dev/urandom -dc A-Za-z0-9 | head -c5
-echo
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
 }
 
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
 	ip64() {
 		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
